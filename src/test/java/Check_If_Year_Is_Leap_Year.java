890c0259d864df6efefe4e6f66e7dff52fd555c8
Dolphinus40 <dolphinus40@hotmail.com>
2022-10-19 19:36:58 +0200

    reporasdasdffedsgdfg

--- src/test/java/Check_If_Year_Is_Leap_Year.java
+++ src/test/java/Check_If_Year_Is_Leap_Year.java
@@ -23,7 +23,7 @@ public class Check_If_Year_Is_Leap_Year {
     @Test
     void Checks_If_Year_Divisible_By_4_But_Not_100_Is_Leap_Year() {
         int yearToTest = 2024;
-        assertTrue(year.isLeapYear(yearToTest+2),
+        assertTrue(year.isLeapYear(yearToTest),
                 "Year " + yearToTest + " dosent check out as leap year, when it actually is a leap year");
     }
 
